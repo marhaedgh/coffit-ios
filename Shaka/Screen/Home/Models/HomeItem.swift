11d69c289425036ef272454eefe06a651bf73077
dannaward <daeunlee0720@gmail.com>
2024-10-21 00:36:07 +0900

    add home view

---
+++ Shaka/Screen/Home/Models/HomeItem.swift
@@ -0,0 +1,16 @@
+//
+//  HomeItem.swift
+//  Shaka
+//
+//  Created by danna.x-PC on 10/20/24.
+//
+
+import Foundation
+
+struct HomeItem {
+    let title: String
+    let summary: String
+    let keywords: [String]
+    let date: Date
+    var isRead: Bool
+}
